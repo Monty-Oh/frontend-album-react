2b647c47edbfd87bc7fa511d505ca5b1c3262882
오한진 <monty@plgrim.com>
2024-09-11 15:41:22 +0900

    Modal CSS 변경

--- src/common/axios.js
+++ src/common/axios.js
@@ -2,7 +2,12 @@ import axios from "axios";
 import {DEFAULT_ERROR_CODE, DEFAULT_ERROR_MESSAGE} from "common/constants";
 import {USER_LOGIN_URL} from "common/urls";
 
-//  로그인 요청
+/**
+ * 로그인 요청
+ *
+ * @param loginId   로그인 ID
+ * @param password 입력된 비밀번호
+ */
 const requestLogin = function (loginId, password) {
     return axios
         .post(
@@ -12,6 +17,10 @@ const requestLogin = function (loginId, password) {
         .catch(errorHandler);
 }
 
+/**
+ * 공통 에러 핸들러
+ * @param error 에러
+ */
 const errorHandler = function (error) {
     const {code = DEFAULT_ERROR_CODE, message = DEFAULT_ERROR_MESSAGE} = error.response.headers;
     const decodedMessage = decodeURI(message).replace(/\+/g, " ");
